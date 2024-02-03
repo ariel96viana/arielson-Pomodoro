82f8fe1e09e091cf5646075ffa90c3a197a4fa0d
Ariel Oliveira <ariel96viana@gmail.com>
2024-02-03 17:21:55 -0300

    Criando o projeto de Pomodoro web

---
+++ main.js
@@ -0,0 +1,58 @@
+const html = document.querySelector("html");
+
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
+const buttons = document.querySelectorAll(".app__card-button");
+const musicFocusInput = document.querySelector("#alternar-musica");
+const music = new Audio("./sons/luna-rise-part-one.mp3");
+music.loop = true;
+
+musicFocusInput.addEventListener("change", () => {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+});
+
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBt.classList.add("active");
+});
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
+});
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
+});
+
+function alterarContexto(contexto) {
+  buttons.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+
+  switch (contexto) {
+    case "foco":
+      title.innerHTML = `Otimize sua produtividade,<br />
+        <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+
+    case "descanso-curto":
+      title.innerHTML = `Que tal dar uma respirada?<br />
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+
+    case "descanso-longo":
+      title.innerHTML = `Hora de voltar à superfície.<br />
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+      break;
+  }
+}
